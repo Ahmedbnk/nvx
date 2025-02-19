b173e51742683cf7c36419f1727a27c9a12a11b8
Ahmed Benkrara <abenkrar@c1r9p3.1337.ma>
2025-02-19 23:55:09 +0100

    commited

--- backend/server.js
+++ backend/server.js
@@ -10,23 +10,19 @@ const DATA_FILE = "./data.json";
 app.use(cors());
 app.use(express.json());
 
-// Load users
 const loadUsers = () => {
   return JSON.parse(fs.readFileSync(DATA_FILE, "utf-8"));
 };
 
-// Save users
 const saveUsers = (users) => {
   fs.writeFileSync(DATA_FILE, JSON.stringify(users, null, 2), "utf-8");
 };
 
-// Get all users
 app.get("/users", (req, res) => {
   const users = loadUsers();
   res.json(users);
 });
 
-// Update votes
 app.post("/vote/:id", (req, res) => {
   const users = loadUsers();
   const userId = parseInt(req.params.id);
